5bc1e5ca2def8aaee9d7587cd5f0f5d517979f3c
adamzzio <120027998+adamzzio@users.noreply.github.com>
2023-05-31 17:49:15 +0700

    Update 02_💬_Feedback.py

--- pages/02_💬_Feedback.py
+++ pages/02_💬_Feedback.py
@@ -7,6 +7,12 @@ import numpy as np
 import streamlit as st
 from streamlit_star_rating import st_star_rating
 
+# for database
+import firebase_admin
+import csv
+import google.cloud
+from firebase_admin import credentials, firestore
+
 # etc
 from PIL import Image
 import requests
@@ -14,6 +20,22 @@ import requests
 # SET PAGE
 pageicon = Image.open("aset_foto/CardioCheck.png")
 st.set_page_config(page_title="CardioCheck Web App", page_icon=pageicon, layout="wide")
+
+def get_firebase_app():
+    # Periksa apakah aplikasi Firebase sudah ada
+    if not firebase_admin._apps:
+        # Jika belum ada, inisialisasi Firebase Admin SDK
+        cred = credentials.Certificate("test-db-19c39-firebase-adminsdk-3noie-95deabbed0.json")
+        firebase_admin.initialize_app(cred)
+    # Kembalikan referensi ke aplikasi Firebase
+    return firebase_admin.get_app()
+
+def save_data_to_firebase_feedback(feedback):
+    app = get_firebase_app()
+    db = firestore.client(app)
+    collection_name = "feedback"
+    doc_ref = db.collection(collection_name).document()
+    doc_ref.set(feedback)
 
 # SET HEADER
 st.header('Formulir Feedback')
@@ -38,4 +60,5 @@ if submit:
                    'Tingkat Kepuasan':option,
                    'Ulasan':ulasan}
 
-    st.write(dict_result)
+    save_data_to_firebase_feedback(dict_result)
+    st.success("Feedback Anda sudah berhasil disimpan ke database")
